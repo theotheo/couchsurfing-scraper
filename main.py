4c573adb14b81db3e67a7df621a2e58926ad7933
I <ibelyalov@yandex.ru>
2017-03-08 15:46:17 +0300

    Improve everything
    Add an argument parser
    Fix multithreading
    Change result files path to nested directory

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 import os
@@ -16,25 +17,40 @@ LOGIN = os.environ.get('LOGIN')
 PASSWORD = os.environ.get('PASSWORD')
 api = Api(LOGIN, PASSWORD)
 
-START = 30000
-COUNT = 120000
-END = START + COUNT
-POOL_SIZE = 16
+parser = argparse.ArgumentParser(description='Scrape couchsurfing.com')
+parser.add_argument('-s', '--start', help='Start from this id', required=True)
+parser.add_argument('-c', '--count', help='Scrape this number ids', required=True)
 
-logging.basicConfig(filename='{}-{}.log'.format(START, END), level=logging.ERROR)
+
+DIR = 'data'  # dir to save results
+POOL_SIZE = 16  # number of parallel processes
+
+# TODO: seems like ugly way to catch multiprocess exceptions
+def get_user(id):
+    try:
+        res = api.get_user(id)
+        return res
+    except Exception as e:
+        logging.error(str(e))
+
 
 if __name__ == '__main__':
-    output = open('users{}-{}.json'.format(START, END), 'w', buffering=10000)
+    args = parser.parse_args()
+    START = args.start
+    COUNT = args.count
+    END = START + COUNT
+    logging.basicConfig(filename='{}-{}.log'.format(START, END), level=logging.ERROR)
+
+    output = open('{}/users{}-{}.json'.format(DIR, START, END), 'w', buffering=10000)
     pool = ThreadPool(POOL_SIZE)
-    for group in tqdm(range(START, END, POOL_SIZE)):
-        ids = list(range(group, group + POOL_SIZE))
-        try:
-            for user in pool.imap(api.get_user, ids):
-                output.write(json.dumps(user))
-                output.write('\n')
+    ids = range(START, END)
+
+    for user in tqdm(pool.imap_unordered(get_user, ids), mininterval=5):
+        if user:
+            # print(user['id'])
+            output.write(json.dumps(user))
+            output.write('\n')
             # output.flush()
-        except Exception as e:
-            logging.error(str(e))
 
     pool.close()
     pool.join()
